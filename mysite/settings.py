2737f38dc65eda6a91b97e567305ff14e96ec0db
Tony <tmaricic1@gmail.com>
2023-10-02 23:04:32 -0400

    changed config

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-env.eba-zcuxba4v.us-west-2.elasticbeanstalk.com']
 
