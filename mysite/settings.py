39f8ebff5715b1f2d4b09f36797957f3f4a81701
Tony <tmaricic1@gmail.com>
2023-10-13 18:52:30 -0400

    added stuff for travis

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["swe1-app-dev2.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["swe1-app-dev2.us-west-2.elasticbeanstalk.com", "127.0.0.1"]
 
 # Application definition
 
